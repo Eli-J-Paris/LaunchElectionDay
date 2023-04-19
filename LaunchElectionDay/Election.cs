e2b5ffa523300c1c2c123c70d6394055c0b5c640
Eli-J-Paris <Eli.J.Paris@gmail.com>
2023-04-19 15:51:49 -0600

    updated code

---
+++ LaunchElectionDay/Election.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchElectionDay
+{
+    public class Election
+    {
+        public string Year;
+        public List<Race> Races;
+        public Election(string year)
+        {
+            Year = year;
+            Races = new List<Race>();
+        }
+
+        public string GetYear()
+        {
+            return Year;
+        }
+
+        public List<Race> GetRaces()
+        {
+            return Races;
+        }
+
+        public void AddRace(Race race)
+        {
+            Races.Add(race);
+        }
+
+        public List<Candidate> GetCandidatesRace(Race race)
+        {
+            List<Candidate> candidates = new List<Candidate>();
+
+            foreach(var canidate in race.Candidates)
+            {
+                candidates.Add(canidate);
+            }
+
+            return candidates;
+        }
+
+        public Dictionary<string,int> GetVoteCountsRace(Race race)
+        {
+            var CandidateVotes = new Dictionary<string, int>();
+
+            foreach(var canidate in race.Candidates)
+            {
+                CandidateVotes.Add(canidate.Name,canidate.Votes);
+            }
+            return CandidateVotes;
+        }
+
+        public List<Candidate> GetAllCandidates()
+        {
+            List<Candidate> candidates = new List<Candidate>();
+
+            foreach (Race race in Races)
+            {
+                foreach(Candidate candidate in race.Candidates)
+                {
+                    candidates.Add(candidate);
+                }
+            }
+
+            return candidates;
+        }
+
+        public Dictionary<string, int> GetAllVoteCounts()
+        {
+            var CandidateVotes = new Dictionary<string, int>();
+
+            foreach (Race race in Races)
+            {
+                foreach(Candidate canidate in race.Candidates)
+                CandidateVotes.Add(canidate.Name, canidate.Votes);
+
+            }
+            return CandidateVotes;
+        }
+
+    }
+}
--- LaunchElectionDay/Program.cs
+++ LaunchElectionDay/Program.cs
@@ -1,14 +1,47 @@
 ﻿using LaunchElectionDay;
 
-//var diana = new Candidate("Diana D", "Democrat");
+var diana = new Candidate("Diana D", "Democrat");
+var JaredPolis = new Candidate("Jared Polis", "Democrat");
 
-//Console.WriteLine(diana.Name);
+Console.WriteLine(diana.Name);
+Console.WriteLine(diana.Party);
+Console.WriteLine(diana.Votes);
 
-//Console.WriteLine(diana.Party);
-//Console.WriteLine(diana.Votes);
+diana.VoteFor();
+diana.VoteFor();
+diana.VoteFor();
 
-//diana.VoteFor();
-//diana.VoteFor();
-//diana.VoteFor();
+Console.WriteLine(diana.Votes);
 
-//Console.WriteLine(diana.Votes);
+Race race1 = new Race("Mayoral Race");
+race1.RegisterCandidate(diana);
+race1.RegisterCandidate(JaredPolis);
+
+Election election1 = new Election("3043");
+election1.AddRace(race1);
+
+//List<Candidate> allCanidatesInRace = election1.GetCandidatesRace(race1);
+//foreach(var c in allCanidates)
+//{
+//    Console.WriteLine(c.Name);
+//}
+
+//var canidateVotes = election1.GetVoteCountsRace(race1);
+
+//foreach(var c in canidateVotes)
+//{
+//    Console.WriteLine($"{c.Key} has {c.Value} votes");
+//}
+
+List<Candidate> allCanidates = election1.GetAllCandidates();
+foreach(var c in allCanidates)
+{
+    Console.WriteLine(c.Name);
+}
+
+var allCanidatesVotes = election1.GetAllVoteCounts();
+foreach (KeyValuePair<string,int> c in allCanidatesVotes)
+{
+    Console.WriteLine($"{c.Key} has {c.Value} votes");
+
+}
